d2855a493f98afa11c51511fee8f6c8ff065b185
mnpkoerner <mnpkoerner@gmail.com>
2021-02-24 14:29:01 -0600

    placeholder

--- script.py
+++ script.py
@@ -5,7 +5,9 @@ app = flask.Flask(__name__)
 app.config["DEBUG"] = True
 
 
-
+@app.route('/', methods=['GET'])
+def home():
+    return "<h1>Hello World!</h1><p>From Python and Flask!</p>"
 
 
 app.run()
